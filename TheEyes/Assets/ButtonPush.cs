bc24c17f6d5ae263e1bf8667962bdab1e8bda7fd
Mohamed fadel Labrayji <mlabrayj@e2r4p12.1337.ma>
2021-01-21 11:27:27 +0100

    theeyes

---
+++ TheEyes/Assets/ButtonPush.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ButtonPush : MonoBehaviour
+{
+    public GameObject Box;
+    private int  k = 0;
+    public int  p = 0;
+    
+
+    void OnMouseDown () {
+        OnMouseUp();
+            transform.position = new Vector2(transform.position.x, 0.3f);
+            k++;
+            NumberOfClicks();
+    }
+    void OnMouseUp()
+    {
+        transform.position = new Vector2(transform.position.x, -3.5f);
+    }
+    void NumberOfClicks(){
+        if (k >= p)
+            Destroy(Box);
+    }
+    
+}
+    
